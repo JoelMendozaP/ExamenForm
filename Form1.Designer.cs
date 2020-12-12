58cac4574f306be929f61b30a1169b6b3b3e9776
JoelMendozaP <joelmendozapaucara@gmail.com>
2020-12-12 14:40:45 -0400

    Proyecto terminado :)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,152 @@ namespace ExamenForm
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(168, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 185);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.BurlyWood;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(12, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 30);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Cargar Imagen";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(168, 221);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 285);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 141);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Ninguna";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Textura Seleccionada:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Nivel de tolerancia";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 34);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Buscar Textura";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 479);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 27);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(988, 525);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExamenForm.Dato;
+using ExamenForm.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +14,161 @@ namespace ExamenForm
 {
     public partial class Form1 : Form
     {
+        int index = 0;
+        DataTable tabla;
+        Bitmap Imagen;
+        TextureData dato = new TextureData();
         public Form1()
         {
             InitializeComponent();
+            Guardar("Lago",0, 14, 17, 44, 32, 65);
+            Guardar("Lago",0, 17, 17, 47, 35, 68);
+            Guardar("Tierra",77, 214, 69, 198, 56, 186);
+            Guardar("Vegetacion",21, 91, 48, 109, 40, 80);
+            Iniciar();
+            Consultar();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CargarImagen();
         }
 
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Imagen == null)
+            {
+                CargarImagen();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int c1 = Int32.Parse((string)dataGridView1.Rows[index].Cells[1].Value);
+            int c2 = Int32.Parse((string)dataGridView1.Rows[index].Cells[2].Value);
+            int c3 = Int32.Parse((string)dataGridView1.Rows[index].Cells[3].Value);
+            int c4 = Int32.Parse((string)dataGridView1.Rows[index].Cells[4].Value);
+            int c5 = Int32.Parse((string)dataGridView1.Rows[index].Cells[5].Value);
+            int c6 = Int32.Parse((string)dataGridView1.Rows[index].Cells[6].Value);
+            if (Imagen == null)
+            {
+                CargarImagen();
+            }
+            else
+            {
+                Pintar(c1, c2, c3, c4, c5, c6);
+            }
+        }
+        private void CargarImagen()
+        {
+            openFileDialog1.Filter = "Imagenes PNG|*.png";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1 != null)
+            {
+                Imagen = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = Imagen;
+            }
+        }
+        private void Iniciar()
+        {
+            tabla = new DataTable();
+            tabla.Columns.Add("Nombre");
+            tabla.Columns.Add("R1");
+            tabla.Columns.Add("R2");
+            tabla.Columns.Add("G1");
+            tabla.Columns.Add("G2");
+            tabla.Columns.Add("B1");
+            tabla.Columns.Add("B2");
+            dataGridView1.DataSource = tabla; 
+        }
+        private void Guardar(string name, int r1,int r2, int g1, int g2, int b1, int b2)
+        {
+            TextureModel modelo = new TextureModel()
+            {
+                name = name,
+                R1 = r1,
+                R2 = r2,
+                G1 = g1,
+                G2 = g2,
+                B1 = b1,
+                B2 = b2,
+            };
+            dato.Guardar(modelo);
+        }
+        private void Consultar()
+        {
+            foreach (var item in dato.Consultar())
+            {
+                DataRow fila = tabla.NewRow();
+                fila["Nombre"] = item.name;
+                fila["R1"] = item.R1;
+                fila["R2"] = item.R2;
+                fila["G1"] = item.G1;
+                fila["G2"] = item.G2;
+                fila["B1"] = item.B1;
+                fila["B2"] = item.B2;
+                tabla.Rows.Add(fila);
+            }
+        }
+        private void Limpiar()
+        {
+            if (Imagen == null)
+            {
+                CargarImagen();
+            }
+            else
+            {
+                pictureBox1.Image = Imagen;
+            }
+        }
+        private void Pintar(int r1, int r2, int g1, int g2, int b1, int b2)
+        {
+            Bitmap b = new Bitmap(pictureBox1.Image);
+            Bitmap bc = new Bitmap(b.Width, b.Height);
+            decimal num = numericUpDown1.Value;
+            Color c = new Color();
+            for (int i = 0; i < b.Width; i++)
+            {
+                for (int j = 0; j < b.Height; j++)
+                {
+                    c = b.GetPixel(i, j);
+                    if ((c.R >= r1 - num && c.R <= r2 + num) && (c.G >= g1 - num && c.G <= g2 + num) && (c.B - num >= b1 && c.B <= b2 + num))
+                    {
+                        for (int k = i; k < i + 5; k++)
+                        {
+                            for (int l = j; l < j + 5; l++)
+                            {
+                                if (k < b.Width && l < b.Height)
+                                    if (k > 0 && l > 0)
+                                    {
+                                        bc.SetPixel(k, l, Color.FromArgb(0, 0, 0));
+                                    }
+                            }
+                        }
+                        bc.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    }
+                    else
+                    {
+                        bc.SetPixel(i, j, Color.FromArgb(c.R, c.G, c.B));
+                    }
+                }
+            }
+            pictureBox1.Image = bc;
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int n = e.RowIndex;
+            if(n != -1)
+            {
+                if (dataGridView1.Rows.Count - 1 > n)
+                {
+                    label1.Text = (string)dataGridView1.Rows[n].Cells[0].Value;
+                    index = n;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
     }
 }
